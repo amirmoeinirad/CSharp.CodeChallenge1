273b072cb15040d6511bf2488e0b50c45fe07460
amirmoeinirad <amirmoinirad@gmail.com>
2025-12-05 01:35:40 +0330

    Add project files.

---
+++ MiniBank.Domain/Entities/Account.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MiniBank.Domain.Entities
+{
+    public class Account
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        
+        [Required]
+        public string CustomerName { get; set; } = null!;
+    }
+}
